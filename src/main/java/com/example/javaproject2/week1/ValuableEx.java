156df169f2d584555a7b6356aa13535583c9bf95
eunhwa0308 <eunhwa0308@naver.com>
2023-04-20 15:17:14 +0900

    feat: ValuableEx 추가 - 변수 선언과 초기화

---
+++ src/main/java/com/example/javaproject2/week1/ValuableEx.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week1;
+
+import com.example.javaproject2.PrintHello;
+
+public class ValuableEx {
+    public static void main(String[] args) {
+        int ival;
+        ival = 0; //최초로 값을 지정하는 것 : 초기화
+
+        PrintHello printHello; //변수 선언
+        printHello = new PrintHello(); //new를 이용해 인스턴스화 및 초기화
+
+        System.out.println(ival);
+        printHello.print();
+    }
+}
