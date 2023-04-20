8b0b31458226ec19c596ad35871ee00f81ac3ebf
eunhwa0308 <eunhwa0308@naver.com>
2023-04-20 15:36:51 +0900

    feat: DefineAndInitialize, ScannerAndVariable 추가 - 변수 선언, 초기화

---
+++ src/main/java/com/example/javaproject2/week1/DefineAndInitialize.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week1;
+
+import com.example.javaproject2.PrintHello;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        int ival = 1;
+        PrintHello printHello = new PrintHello(); //변선 선언 + new로 초기화
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/ScannerAndVariable.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerAndVariable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ival = sc.nextInt(); //변수 선언 + 초기화
+        System.out.println(ival);
+    }
+}
