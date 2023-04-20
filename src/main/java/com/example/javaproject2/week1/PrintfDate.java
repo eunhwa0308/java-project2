0cd02b9a8b4ce14dab5eb3ba5510fe3298819bba
eunhwa0308 <eunhwa0308@naver.com>
2023-04-20 13:32:54 +0900

    feat: PrintfEx,PrintTwoValuesShortage,PrintDate 추가 - printf 실습

---
+++ src/main/java/com/example/javaproject2/week1/PrintfDate.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1;
+
+public class PrintfDate {
+    public static void main(String[] args) {
+        System.out.printf("%02d\n",4);
+        System.out.printf("%02d\n",11);
+        System.out.printf("%d-%02d-%02d",2023,4,20);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/PrintfEx.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week1;
+
+public class PrintfEx {
+
+    public void print(){
+        System.out.printf("%s","Hello\n");
+        System.out.println("Bye");
+    }
+    public static void main(String[] args) {
+        PrintfEx printfEx = new PrintfEx(); //PrintEx나와라
+        printfEx.print();
+    }
+}
