6539610d626f053a128c29ace0d0d6798f3cf3ba
eunhwa0308 <eunhwa0308@naver.com>
2023-04-24 18:25:51 +0900

    feat : 자바 타입 - SpringPlusInt,FloatDigit,FloatAndDoubles, DivideIntoTwo,Parse

---
+++ src/main/java/com/example/javaproject2/week2/FloatDigit.java
@@ -0,0 +1,26 @@
+package com.example.javaproject2.week2;
+
+public class FloatDigit {
+    public static void main(String[] args) {
+
+        float f5 = 1.23e5f;
+        float f6 = 1.23e6f;
+        float f7 = 1.23e7f;
+        float f8 = 1.23e8f;
+        float f9 = 1.23e9f;
+        float f10 = 1.23e10f;
+        double d10 = 1.23e10;
+        float f11 = 3.141592653589793f;
+        double d1 = 3.141592653589793;
+
+        System.out.println("f7 = " + f7);
+        System.out.println("f8 = " + f8);
+        System.out.println("f9 = " + f9);
+        System.out.println("f10 = " + f10);
+        System.out.println("d10 = " + d10);
+
+        System.out.println(f11);
+        System.out.println(d1);
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/SpringPlusInt.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week2;
+
+public class SpringPlusInt {
+    public static void main(String[] args) {
+        System.out.println("1"+1);
+        String result = "1"+1;
+        //int result ="1" +1; 에러
+        System.out.println("result = "+result);
+
+        int iResult = 1+ Integer.parseInt("1");
+        System.out.println("iResult = "+iResult);
+
+
+    }
+}
