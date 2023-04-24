cecce2e666edff6371a57f7d3e5c924faef4461e
eunhwa0308 <eunhwa0308@naver.com>
2023-04-24 18:28:28 +0900

    feat : 자바 타입 - BooleanEx

---
+++ src/main/java/com/example/javaproject2/week2/BooleanEx.java
@@ -0,0 +1,19 @@
+package com.example.javaproject2.week2;
+
+public class BooleanEx {
+    public static void main(String[] args) {
+        boolean flag1 = false;
+        boolean flag2 = true;
+
+        boolean isBalanceSufficient = true;
+        boolean isPaymentSuccess = false;
+
+        boolean result1 = isBalanceSufficient == true;
+        boolean result2 = isPaymentSuccess == true;
+
+        System.out.println("result1: "+result1);
+        System.out.println("result2: "+result2);
+
+
+    }
+}
