beab19f3f0f14487c7a641d40e0e5386e20ad253
eunhwa0308 <eunhwa0308@naver.com>
2023-04-24 09:16:01 +0900

    feat : week1 day5 - ConstantEx - Codeup1018

---
+++ src/main/java/com/example/javaproject2/week1/day5/ConstantEx.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1.day5;
+
+import com.example.javaproject2.PrintHello;
+
+public class ConstantEx {
+    public static void main(String[] args) {
+        final int iVal;
+        iVal = 1;
+        System.out.println(iVal);
+
+        final PrintHello printHello = new PrintHello();
+
+    }
+}
