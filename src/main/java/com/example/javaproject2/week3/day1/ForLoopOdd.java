d1267de22b74ed8d80f951d78d99bd819a9bc7a4
eunhwa0308 <eunhwa0308@naver.com>
2023-05-02 09:15:14 +0900

    feat : week3-day1

---
+++ src/main/java/com/example/javaproject2/week3/day1/ForLoopOdd.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week3.day1;
+
+public class ForLoopOdd {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i+=2) {
+            System.out.println(i);
+        }
+
+    }
+}
